7fd60abf50e665acf388de2440d5b541ea50927b
NateScarlet <NateScarlet@Gmail.com>
2019-03-11 21:07:36 +0800

    Add push script

---
+++ push.js
@@ -0,0 +1,6 @@
+const { execSync } = require('child_process');
+const version = require('./package.json').version;
+
+execSync(`adb push dist/auto-dragalia-${version}.auto.js /sdcard/脚本/`, {
+  stdio: 'inherit'
+});
