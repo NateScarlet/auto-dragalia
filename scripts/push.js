3e02b83d04f79280166b70652cb68d165fb8f31e
NateScarlet <NateScarlet@Gmail.com>
2019-04-06 15:01:10 +0800

    chore: move files to scripts folder

--- scripts/push.js
+++ scripts/push.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
-const version = require('./package.json').version;
+const version = require('../package.json').version;
 
 execSync(`adb push dist/auto-dragalia-${version}.auto.js /sdcard/脚本/`, {
   stdio: 'inherit'
