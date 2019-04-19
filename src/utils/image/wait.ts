c2ad3c398e0fe302bf3f409ebe380ed3fc0a4416
NateScarlet <NateScarlet@Gmail.com>
2019-04-20 04:18:07 +0800

    refactor: improve `waitAnyImage`

--- src/utils/image/wait.ts
+++ src/utils/image/wait.ts
@@ -19,30 +19,54 @@ export async function waitAnyImage(
   images: Image[],
   options?: IWaitImageOptions
 ): Promise<Point | void> {
+  const { timeout = 600e3 } = options || {};
+  const waitingEndTime: number = Date.now() + timeout;
+  let ret: Point | undefined;
   waitingCount += 1;
+
+  for (
+    let isFinished: boolean = false;
+    !isFinished;
+    isFinished = Boolean(ret) === appear
+  ) {
+    ret = await handleAnyImageWaiting(appear, images, options, waitingEndTime);
+  }
+
+  return ret;
+}
+
+async function handleAnyImageWaiting(
+  appear: boolean,
+  images: Image[],
+  options: IWaitImageOptions | undefined,
+  waitingEndTime: number
+): Promise<Point | undefined> {
   const {
-    timeout = 600e3,
     delay = 500,
     findOptions = {},
     onDelay = (): void | Promise<void> => undefined,
     id = String(waitingCount)
   } = options || {};
 
-  const waitingEndTime: number = Date.now() + timeout;
-  while (Date.now() < waitingEndTime) {
-    const roundStartTime: number = Date.now();
-    const ret: Point | undefined = tryFindAnyImage(images, {
-      ...findOptions,
-      id
-    });
-    if (Boolean(ret) === appear) {
-      return ret;
-    }
-    console.verbose(`Waiting image ${appear ? 'appear' : 'disappear'}: ${id}`);
-    await onDelay();
-    await wait(delay - (Date.now() - roundStartTime));
+  if (Date.now() > waitingEndTime) {
+    throw new Error('等待超时');
   }
-  throw new Error('等待超时');
+
+  const roundStartTime: number = Date.now();
+  const ret: Point | undefined = tryFindAnyImage(images, {
+    ...findOptions,
+    id
+  });
+
+  if (Boolean(ret) === appear) {
+    return ret;
+  }
+
+  console.verbose(`Waiting image ${appear ? 'appear' : 'disappear'}: ${id}`);
+  await onDelay();
+  await wait(delay - (Date.now() - roundStartTime));
+
+  return ret;
 }
 
 export async function waitImage(
