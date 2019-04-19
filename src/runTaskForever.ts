ba96d5049d4526c1dea0543fe063fcfe3e909bf4
NateScarlet <NateScarlet@Gmail.com>
2019-04-20 02:34:16 +0800

    refactor: improve runTaskForever.ts

--- src/runTaskForever.ts
+++ src/runTaskForever.ts
@@ -4,22 +4,27 @@ import { wait } from '@/utils/wait';
 import { version } from 'package.json';
 
 export async function runTaskForever(): Promise<void> {
-  if (store.currentTask !== undefined) {
-    const taskName: string = store.currentTask;
-    const handler: (() => Promise<void> | void) | undefined =
-      taskRegistry[taskName];
-    if (!handler) {
-      throw new Error(`Unknown task: ${taskName}`);
-    }
-    console.log(`运行任务: ${taskName}`);
-    try {
-      await handler();
-    } catch (err) {
-      console.show();
-      console.error(`${version}: ${err}`);
-      device.vibrate(0.5e3);
-      store.currentTask = undefined;
-    }
+  if (!store.currentTask) {
+    await wait(1000);
+    await runTaskForever();
+
+    return;
+  }
+
+  const handler: (() => Promise<void> | void) | undefined =
+    taskRegistry[store.currentTask];
+  if (!handler) {
+    throw new Error(`Unknown task: ${store.currentTask}`);
+  }
+
+  console.log(`运行任务: ${store.currentTask}`);
+  try {
+    await handler();
+  } catch (err) {
+    console.show();
+    console.error(`${version}: ${err}`);
+    device.vibrate(0.5e3);
+    store.currentTask = undefined;
   }
   await wait(1000);
   await runTaskForever();
