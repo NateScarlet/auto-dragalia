199f89ae40caf766fedb0224ea8eca3c849d87e6
NateScarlet <NateScarlet@Gmail.com>
2019-09-05 23:41:35 +0800

    fix(farm-rare-item): add more matching image
    fix #126

--- src/assets/images/1080x2340/index.ts
+++ src/assets/images/1080x2340/index.ts
@@ -1,9 +1,11 @@
 // This file is auto generated
 // Use `npm run code-generate:images` to update this file
 import * as noRareItem1Png from '@/assets/images/1080x2340/no-rare-item-1.png';
+import * as noRareItem2Png from '@/assets/images/1080x2340/no-rare-item-2.png';
 
 export const index: Record<string, Image> = {};
 
 index.noRareItem1 = images.fromBase64(noRareItem1Png);
+index.noRareItem2 = images.fromBase64(noRareItem2Png);
 
 Object.freeze(index);
