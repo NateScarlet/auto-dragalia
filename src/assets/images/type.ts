f208bb81600757d8579c39a08b70810d5383fda0
NateScarlet <NateScarlet@Gmail.com>
2019-09-07 16:14:23 +0800

    chore: remove unused asset

--- src/assets/images/type.ts
+++ src/assets/images/type.ts
@@ -20,8 +20,6 @@ export type ImageAssets = Record<
   | 'lv29Button'
   | 'menuButton'
   | 'nextText'
-  | 'noRareItem1'
-  | 'noRareItem2'
   | 'okButton'
   | 'presentButton'
   | 'presentPrice0'
